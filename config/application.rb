bcfc8fb981b41cf770b5640b83a992b5722f56e2
Sarah Rohm <sarahjrohm@gmail.com>
2011-06-26 23:08:55 -0400

    added files to Production for Heroku

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module Enki
   class Application < Rails::Application
      config.autoload_paths += %W( #{config.root}/app/models/ckeditor )
+     Paperclip::Railtie.insert
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
