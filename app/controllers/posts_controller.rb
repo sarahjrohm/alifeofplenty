786ca5da87c8bb2114d56d023f58d0f85048e75e
Sarah Rohm <sarahjrohm@gmail.com>
2011-06-27 22:31:37 -0400

    Tags working with paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @tag = params[:tag]
-    @posts = Post.paginate :page => params[:page], :order => 'published_at DESC', :per_page=>5 
+    @posts = Post.find_recent(:tag => @tag, :include => :tags).paginate(:page => params[:page], :order => 'published_at DESC', :per_page=>5)
 
     respond_to do |format|
       format.html
