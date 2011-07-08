b6debde0df48ebb2bde1a848edeb580872853a0c
Sarah Rohm <sarahjrohm@gmail.com>
2011-07-09 00:21:16 +0200

    added slideshow

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,14 +6,18 @@ module NavigationHelper
      link.new("Trip Map", map_path),
      link.new("About", about_path),
      link.new("Contact", contact_path),
-     link.new("Archives", archives_path)] +
-      Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page.slug))}
+     link.new("Archives", archives_path)] 
   end
 
   def category_links_for_navigation
     link = Struct.new(:name, :url)
     @popular_tags ||= Tag.find(:all).reject {|tag| tag.taggings.empty? }.sort_by {|tag| tag.taggings.size }.reverse
     @popular_tags.collect {|tag| link.new(tag.name, posts_path(:tag => tag.name)) }
+  end
+  
+    def picture_links_for_navigation
+    link = Struct.new(:name, :url)
+      Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page.slug))}
   end
 
   def class_for_tab(tab_name, index)
